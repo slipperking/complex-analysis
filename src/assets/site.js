836e531633d16dbe5a669ca74e3fcbb1282e8c8a
slipperking <183034529+slipperking@users.noreply.github.com>
2026-06-26 14:41:29 -0700

    Update site.js

--- src/assets/site.js
+++ src/assets/site.js
@@ -151,7 +151,7 @@
 
       var top = anchors.reduce(function (sum, anchor) {
         var rect = anchor.getBoundingClientRect();
-        return sum + rect.top - wrapperRect.top - (parseFloat(getComputedStyle(group).lineHeight) || 0) / 2;
+        return sum + rect.top - wrapperRect.top - (parseFloat(getComputedStyle(group).fontSize) || 0) / 2;
 }, 0) / anchors.length;
 
       group.style.top = top + "px";
